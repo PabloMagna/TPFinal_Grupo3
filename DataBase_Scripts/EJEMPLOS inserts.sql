c9b67935aaefd6b4c81fc4efea36b27dc53d039e
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2023-06-18 22:04:28 +0200

    corregido DB y Ejemplos par que se pueda probar

--- DataBase_Scripts/EJEMPLOS inserts.sql
+++ DataBase_Scripts/EJEMPLOS inserts.sql
@@ -1,19 +1,18 @@
 
-use DBTPFinalGRUPO3
 -- Inserts para la tabla Usuarios
-INSERT INTO Usuarios (IDTipoUsuario, Contrasenia, UrlImagen, Email, IDLocalidad, IDProvincia, Telefono, Estado, EsAdmin) VALUES
-(1, 'contraseña1', 'https://img.freepik.com/foto-gratis/personas-que-sonrie-alegre-hombres-guapos_1187-6057.jpg', 'usuario1@example.com', 1, 1, '1234567890', 1, 0),
-(1, 'contraseña2', 'https://example.com/imagen2.jpg', 'usuario2@example.com', 2, 1, '0987654321', 1, 0),
-(2, 'contraseña3', 'https://thumbs.dreamstime.com/b/familia-que-se-familiariza-con-perros-en-refugio-para-animales-123287981.jpg', 'admin@example.com', 3, 2, '9876543210', 1, 1);
+INSERT INTO Usuarios (IDTipoUsuario, Contrasenia, Email, Estado, EsAdmin) VALUES
+(1, 'contraseña1', 'usuario1@example.com', 1, 0),
+(1, 'contraseña2', 'usuario2@example.com', 1, 0),
+(2, 'contraseña3', 'admin@example.com', 1, 1);
 
 -- Inserts para la tabla Personas
-INSERT INTO Personas (IDUsuario, Dni, Nombre, Apellido, FechaNacimiento) VALUES
-(1, 12345678, 'Juan', 'Pérez', '1990-01-01'),
-(2, 23456789, 'María', 'González', '1985-05-10');
+INSERT INTO Personas (IDUsuario, Dni, Nombre, Apellido, FechaNacimiento, UrlImagen, IDLocalidad, IDProvincia, Telefono) VALUES
+(1, 12345678, 'Juan', 'Pérez', '1990-01-01', 'https://img.freepik.com/foto-gratis/personas-que-sonrie-alegre-hombres-guapos_1187-6057.jpg', 1, 1, '1234567890'),
+(2, 23456789, 'María', 'González', '1985-05-10', 'https://example.com/imagen2.jpg', 2, 1, '0987654321');
 
 -- Inserts para la tabla Refugios
-INSERT INTO Refugios (IDUsuario, Direccion, Nombre) VALUES
-(3, 'Calle 123', 'Refugio Animal');
+INSERT INTO Refugios (IDUsuario, Direccion, Nombre, UrlImagen, IDLocalidad, IDProvincia, Telefono) VALUES
+(3, 'Calle 123', 'Refugio Animal', 'https://thumbs.dreamstime.com/b/familia-que-se-familiariza-con-perros-en-refugio-para-animales-123287981.jpg', 3, 2, '9876543210');
 
 -- Inserts para la tabla Mascotas
 INSERT INTO Mascotas (Especie, Raza, Edad, Sexo, Descripcion, Estado) VALUES
@@ -21,9 +20,8 @@ INSERT INTO Mascotas (Especie, Raza, Edad, Sexo, Descripcion, Estado) VALUES
 ('Gato', 'Persa', 1, 'H', 'Es un gato muy tranquilo y cariñoso.', 1),
 ('Perro', 'Bulldog Francés', 3, 'M', 'Es un perro de tamaño pequeño y muy sociable.', 1);
 
-
--- Inserts para la tabla Publicacion
-INSERT INTO Publicacion (IDMascota, IDUsuario, Descripcion, Titulo, FechaHora, Estado) VALUES
+-- Inserts para la tabla Publicaciones
+INSERT INTO Publicaciones (IDMascota, IDUsuario, Descripcion, Titulo, FechaHora, Estado) VALUES
 (1, 1, 'Buscando un hogar para este adorable perro.', 'Adopción de perro', '2023-06-15 10:00:00', 1),
 (2, 2, 'Buscando un hogar para este hermoso gato.', 'Adopción de gato', '2023-06-15 11:00:00', 1),
 (3, 3, 'Buscando un hogar para este simpático perro.', 'Adopción de perro', '2023-06-15 12:00:00', 1);
@@ -40,8 +38,8 @@ INSERT INTO Comentarios (IDPublicacion, Descripcion, Estado) VALUES
 (2, 'Me encanta este gato, es muy tierno.', 1),
 (3, 'Este perro es genial, ojalá encuentre un hogar donde lo cuiden mucho.', 1);
 
--- Inserts para la tabla Historia
-INSERT INTO Historia (IDUsuario, Descripcion, UrlImagen, FechaHora, Estado) VALUES
+-- Inserts para la tabla Historias
+INSERT INTO Historias (IDUsuario, Descripcion, UrlImagen, FechaHora, Estado) VALUES
 (1, 'Hoy adopté a un perro y estoy muy feliz.', 'https://www.petclic.es/wikipets/wp-content/uploads/sites/default/files/library/dalmata_-_razas_de_perro.jpg', '2023-06-14 15:00:00', 1),
 (2, 'Rescaté a un gatito de la calle, ahora tiene un hogar.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Gato_%282%29_REFON.jpg/1600px-Gato_%282%29_REFON.jpg', '2023-06-14 16:00:00', 1),
 (3, 'Visitamos un refugio y conocimos a muchos animales que necesitan adopción.', 'https://example.com/imagen_historia3.jpg', '2023-06-14 17:00:00', 1);
