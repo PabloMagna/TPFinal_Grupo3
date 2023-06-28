fc8accd80a7f0460a8cbfbf0e1abb1adc1afb42e
JoaJufri <joaquinjufrida@gmail.com>
2023-06-28 13:05:27 -0300

    Se agregan clases ImagenUsuario e ImagenUsuarioNegocio

---
+++ Dominio/ImagenUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ImagenUsuario
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public string urlImagen { get; set; }
+
+        public override string ToString()
+        {
+            return this.urlImagen.ToString();
+        }
+    }
+}
